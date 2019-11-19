d9ee0e2732c1eacb96ede9f25a74e48547a2f468
joe <kinjoyuji@gmail.com>
2019-11-19 16:31:01 +1100

    fix

--- bin/codedeploy/after_install.sh
+++ bin/codedeploy/after_install.sh
@@ -5,9 +5,12 @@ set -ex
 project=my_rails6
 user=ec2-user
 
+mkdir -p /mnt/app 
+
 sudo chown -R $user:$user /mnt/app
 
 mkdir -p /mnt/app/${project}/tmp/pids /mnt/app/${project}/tmp/sockets /mnt/app/${project}/tmp/puma
+
 cd /mnt/app/${project}
 
 bundle install --path=vendor/bundle --deployment --without test development --with production
