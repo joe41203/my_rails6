0a99e63f5562afeae2b0e797ddfece5e4be8939f
joe <kinjoyuji@gmail.com>
2019-11-19 18:59:59 +1100

    fix before_install

--- bin/codedeploy/before_install.sh
+++ bin/codedeploy/before_install.sh
@@ -3,8 +3,6 @@
 project=my_rails6
 user=ec2-user
 
-sudo mkdir -p /mnt/app 
+mkdir -p /mnt/app/${project}/tmp/pids /mnt/app/${project}/tmp/sockets /mnt/app/${project}/tmp/puma
 
 sudo chown -R $user:$user /mnt/app
-
-mkdir -p /mnt/app/${project}/tmp/pids /mnt/app/${project}/tmp/sockets /mnt/app/${project}/tmp/puma
